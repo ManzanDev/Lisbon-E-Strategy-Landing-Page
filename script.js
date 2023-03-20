09c037a3a049da0e8e54af322648792b7eab2217
Thess <trmanzan@gmail.com>
2023-03-19 21:39:59 -0300

    lisbon

---
+++ script.js
@@ -0,0 +1,11 @@
+
+JavaScript:
+
+javascript
+const menu = document.querySelector('.menu');
+const NavMenu = document.querySelector('.nav-menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('ativo');
+    NavMenu.classList.toggle('ativo');
+})
